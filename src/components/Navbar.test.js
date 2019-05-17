4686b43145b7488a71793054f475015fb3c85c93
Angel Altamirano <angel.g.altam@gmail.com>
2019-05-17 00:40:31 -0400

    test Navbar component

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import store from '../store';
+import {setAuthToken} from '../actions';
+import {Navbar} from './Navbar';
+
+const mockSetAuthTokenAction = {
+    type: 'SET_AUTH_TOKEN'
+};
+
+jest.mock('../actions', () => Object.assign({},
+    require.requireActual('../actions'),{
+      setAuthToken: jest.fn().mockImplementation(() => {
+            return mockSetAuthTokenAction;
+      })
+    }
+));
+
+const history = {location: {pathname: ""}};
+
+describe('<Navbar/>', () => {
+ it('Renders without crashing', () => {
+    const dispatch = jest.fn();
+    shallow(<Navbar dispatch={dispatch} history={history}/>);
+  });
+
+    it('Has a showFaqs function', () => {
+      const wrapper = shallow(<Navbar history={history}/>);
+      expect(wrapper.instance().showFaqs).toBeDefined();
+  });
+
+  it('Dispatches setAuthToken action when setAuthToken runs.', () => {
+    const dispatch = jest.fn();
+    const wrapper = shallow(<Navbar dispatch={dispatch} history={history}/>);
+    dispatch.mockClear();
+
+    const instance = wrapper.instance();
+    const token = "";
+    instance.setAuthToken(token);
+    expect(dispatch).toHaveBeenCalledWith(mockSetAuthTokenAction);
+  });
+});

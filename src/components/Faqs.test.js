ec7289c217fc33f9b5a9df8d88ce858c570693a0
Angel Altamirano <angel.g.altam@gmail.com>
2019-05-17 20:58:15 -0400

    test Faqs components closeModal functionality

--- src/components/Faqs.test.js
+++ src/components/Faqs.test.js
@@ -6,8 +6,12 @@ describe('<Faqs/>', () => {
 	it('Renders without crashing', () => {
       shallow(<Faqs />);
 	});
-	it('Has a closeModal function', () => {
-			const wrapper = shallow(<Faqs />);
-			expect(wrapper.instance().closeModal).toBeDefined();
-	});
+  it('Runs closeModal when x is clicked.', () => {
+    const closeModal = jest.spyOn(Faqs.prototype, 'closeModal');
+    const wrapper = shallow(<Faqs/>);
+
+    wrapper.find('.close-modal').simulate('click');
+    wrapper.update();
+    expect(closeModal).toHaveBeenCalled();
+  });
 });

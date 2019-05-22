7b7063664efa5c6bd69df432573c3827e77f023d
Angel Altamirano <angel.g.altam@gmail.com>
2019-05-21 22:52:30 -0400

    css tweaks

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,48 +6,50 @@ import {setFormErrors} from '../actions';
 
 export class Form extends Component{
   constructor(props) {
-      super(props);
-      this.onSubmit = this.onSubmit.bind(this);
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onSubmit(e) {
-      e.preventDefault();
-      const username = this.usernameInput.value.trim();
-      const password = this.passwordInput.value.trim();
-      if (username && password && this.props.onAdd) {
-          this.props.onAdd(this.usernameInput.value, this.passwordInput.value);
-      }
-      this.usernameInput.value = '';
-      this.passwordInput.value = '';
-      this.props.dispatch(setFormErrors(""));
+    e.preventDefault();
+    const username = this.usernameInput.value.trim();
+    const password = this.passwordInput.value.trim();
+    if (username && password && this.props.onAdd) {
+      this.props.onAdd(this.usernameInput.value, this.passwordInput.value);
+    }
+    this.usernameInput.value = '';
+    this.passwordInput.value = '';
+    this.props.dispatch(setFormErrors(""));
   }
 
   componentDidMount(){
-      this.props.dispatch(setFormErrors(""));
+    this.props.dispatch(setFormErrors(""));
   }
 
 	render(){
-      if(this.props.authToken){
-          return <Redirect to="/exercise-list"/>;
-      }
-	    return (
-        <div className="landing">
-            <div className="landing-content">
+    if(this.props.authToken){
+        return <Redirect to="/exercise-list"/>;
+    }
+    return (
+      <div className="landing">
+        <div className="landing-content">
+          <div className="landing-text">
             <p>This is a tool for people running a 5/3/1 progression on a lift or following a 5/3/1 program. Now you can stay on track without pulling out your calculator and taking notes!</p>
             <p style={{"textAlign": "center"}}>Happy lifting.</p>
-            </div>
-            <form onSubmit={this.onSubmit}>
-              <h1>{this.props.title}</h1>
-              <p className="error">{this.props.formErrors}</p>
-              <label htmlFor="username-input">Username:</label>
-              <input type="text" name="username" id="username-input" required ref={input => this.usernameInput = input}/>
-              <br/>
-              <label htmlFor="password-input">Password:</label>
-              <input type="password" name="password" id="password-input" required ref={input => this.passwordInput = input}/>
-              <br/>
-              <button className="red-btn" type="submit">Submit</button>
-            </form>
+          </div>
         </div>
+        <form onSubmit={this.onSubmit}>
+          <h1>{this.props.title}</h1>
+          <p className="error">{this.props.formErrors}</p>
+          <label htmlFor="username-input">Username:</label>
+          <input type="text" name="username" id="username-input" required ref={input => this.usernameInput = input}/>
+          <br/>
+          <label htmlFor="password-input">Password:</label>
+          <input type="password" name="password" id="password-input" required ref={input => this.passwordInput = input}/>
+          <br/>
+          <button className="red-btn" type="submit">Submit</button>
+        </form>
+      </div>
 		);
 	}
 
